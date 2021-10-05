8cc6ffa24a3153265aac9fdaef4a67f99eb99449
Wookjin Choi <wchoi@vsu.edu>
2021-10-05 17:28:04 -0400

    Update PathCNN_GradCAM.py
    Clean up the code

--- PathCNN_GradCAM.py
+++ PathCNN_GradCAM.py
@@ -2,81 +2,10 @@ import os
 import pandas as pd
 import numpy as np
 import keras.backend as K
-import tensorflow as tf
 import keras
 import cv2
 
-from scipy.cluster.hierarchy import dendrogram, linkage
 from keras.models import load_model
-from tensorflow.python.framework import ops
-
-def target_category_loss(x, category_index, nb_classes):
-    return tf.multiply(x, K.one_hot([category_index], nb_classes))
-
-def target_category_loss_output_shape(input_shape):
-    return input_shape
-
-def normalize(x):
-    # utility function to normalize a tensor by its L2 norm
-    return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
-
-def register_gradient():
-    if "GuidedBackProp" not in ops._gradient_registry._registry:
-        @ops.RegisterGradient("GuidedBackProp")
-        def _GuidedBackProp(op, grad):
-            dtype = op.inputs[0].dtype
-            return grad * tf.cast(grad > 0., dtype) * \
-                tf.cast(op.inputs[0] > 0., dtype)
-
-def compile_saliency_function(model, activation_layer):
-    input_img = model.input
-    layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
-    layer_output = layer_dict[activation_layer].output
-    max_output = K.max(layer_output, axis=3)
-    saliency = K.gradients(K.sum(max_output), input_img)[0]
-    return K.function([input_img, K.learning_phase()], [saliency])
-
-def modify_backprop(model, name):
-    g = tf.get_default_graph()
-    with g.gradient_override_map({'Relu': name}):
-
-        # get layers that have an activation
-        layer_dict = [layer for layer in model.layers[1:]
-                      if hasattr(layer, 'activation')]
-
-        # replace relu activation
-        for layer in layer_dict:
-            if layer.activation == keras.activations.relu:
-                layer.activation = tf.nn.relu
-
-        # re-instanciate a new model
-        model_file = 'my_model.h5'
-        new_model = load_model(model_file)
-    return new_model
-
-def deprocess_image(x):
-    '''
-    Same normalization as in:
-    https://github.com/fchollet/keras/blob/master/examples/conv_filter_visualization.py
-    '''
-    if np.ndim(x) > 3:
-        x = np.squeeze(x)
-    # normalize tensor: center on 0., ensure std is 0.1
-    x -= x.mean()
-    x /= (x.std() + 1e-5)
-    x *= 0.1
-
-    # clip to [0, 1]
-    x += 0.5
-    x = np.clip(x, 0, 1)
-
-    # convert to RGB array
-    x *= 255
-    if K.image_dim_ordering() == 'th':
-        x = x.transpose((1, 2, 0))
-    x = np.clip(x, 0, 255).astype('uint8')
-    return x
-
 
 def grad_cam(model, category_index, layer_name):
     print(layer_name)
